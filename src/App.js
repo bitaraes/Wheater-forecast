abdd8322a9bd305603c9c3d70d6785a568b73878
Bitarães <igor_oliveira02@hotmail.com>
2021-05-06 07:00:38 -0300

    Submit with enter key

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ export default function App() {
         description: "Céu Limpo"
     });
 
-    async function onSearch() {
-        if (cityName === "") {
-            return (
-                alert("O campo cidade deve ser preenchido")
-            );
-        };
+    function onPressEnter(e) {
+        if (e.key === "Enter") {
+            onSearch();
+        }
+    }
 
+    async function onSearch() {
+        if (!cityName) {
+            return (
+                alert("O nome não pode ficar em branco")
+            )
+        }
         /*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
         const apiKey = "a376594a6a77d7d74ea5a2de80effcc1";
         const filters = "&lang=pt_br&units=metric";
         const url = "https://api.openweathermap.org/data/2.5/weather?q=";
         let urlRequest = await fetch(url + cityName + "&appid=" + apiKey + filters);
-        console.log(urlRequest.url)
         let responseJson = await urlRequest.json();
         if (responseJson.cod === "404") {
             return (
@@ -37,10 +41,12 @@ export default function App() {
             icon: responseJson.weather[0].icon,
             description: responseJson.weather[0].description
         });
+
+        setCity("")
     }
 
     return (
-        <div>
+        <div onKeyPress={onPressEnter} >
             <Input value={cityName} onChange={(e) => setCity(e.target.value)} />
             <Submit onClick={onSearch} />
             <Weather clima={clima} />
--- src/components/input.js
+++ src/components/input.js
@@ -4,7 +4,7 @@ import './../styles/style.css';
 export default function Input(props) {
     return (
         <>
-            <input onChange={props.onChange} type="text" name="search-txt" id="search-txt" placeholder="Digite o nome da cidade"></input>
+            <input onChange={props.onChange} value={props.value} type="text" name="search-txt" id="search-txt" placeholder="Digite o nome da cidade"></input>
         </>
     );
 };
